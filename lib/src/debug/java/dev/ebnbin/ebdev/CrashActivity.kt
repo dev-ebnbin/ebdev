4e5ba63a4b9b8226fc9671b7d55324b13c6259c4
Ebn Zhang <ebnbin@gmail.com>
2021-04-15 23:20:45 +0800

    Disable DataBinding

--- lib/src/debug/java/dev/ebnbin/ebdev/CrashActivity.kt
+++ lib/src/debug/java/dev/ebnbin/ebdev/CrashActivity.kt
@@ -3,7 +3,6 @@ package dev.ebnbin.ebdev
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import cat.ereza.customactivityoncrash.CustomActivityOnCrash
 import dev.ebnbin.eb.appIcon
 import dev.ebnbin.eb.appLabel
@@ -24,17 +23,19 @@ internal class CrashActivity : AppCompatActivity() {
             finish()
             return
         }
-        val binding = DataBindingUtil.setContentView<EbdevCrashActivityBinding>(this, R.layout.ebdev_crash_activity)
-        binding.lifecycleOwner = this
+        val binding = EbdevCrashActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         binding.ebdevIcon.setImageDrawable(appIcon)
         binding.ebdevTitle.text = getString(R.string.ebdev_crash_title, appLabel)
+        viewModel.log.observe(this) {
+            binding.ebdevLog.text = it
+        }
         binding.ebdevClose.setOnClickListener {
             CustomActivityOnCrash.closeApplication(this, caocConfig)
         }
         binding.ebdevRestart.setOnClickListener {
             CustomActivityOnCrash.restartApplication(this, caocConfig)
         }
-        binding.viewModel = viewModel
         if (viewModel.log.value == null) {
             viewModel.log.value = CustomActivityOnCrash.getStackTraceFromIntent(intent)
         }
